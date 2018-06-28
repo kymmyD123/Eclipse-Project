bf4b7c5f25a0a78b4b1cf9e969e90313f5a84c55
Valera <kymmyd123@gmail.com>
2018-06-28 13:02:10 +0300

    закончен

--- js/script.js
+++ js/script.js
@@ -1,20 +1,11 @@
 $(function () {
 
-    //up button scroll top
-    // var options = {
-    //     class: '',
-    //     positionY: 'bottom',
-    //     positionX: 'left',
-    //     text: 'Up',
-    //     displayAlways: false
-    // };
-    //
-    // new Up(options);
 
     $('body').append('<button class="btn_up"></button>');
-
     $('.btn_up').click(function () {
         $('body').animate({'scrollTop': 0}, 1000);
         $('html').animate({'scrollTop': 0}, 1000);
     });
+
+    $('body').append('<button class="contact-us__pop-up">Contact Us</button>');
 });
